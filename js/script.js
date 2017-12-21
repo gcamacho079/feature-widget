e3bf698da36fab18280ea386dbfe484e8403be0f
Lupe <lupe@acoorg.com>
2017-12-21 12:29:33 -0800

    Fix padding for buttons instead of icons, improve spacing

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 var featureWidget = {
   init: function() {
     $(".tablinks").click(function() {
+      $(".features__content").removeClass("features__content--active");
       let showThisContent = "#" + $(this).attr("data-toggler");
-      console.log(showThisContent);
       $(showThisContent).addClass("features__content--active");
     });
   }
 }
-
 
 $(document).ready(featureWidget.init);
