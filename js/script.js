7493e054bdfcc257c7fc52eea42a23442bb28430
Lupe <lupe@acoorg.com>
2017-12-28 09:40:19 -0800

    Implement image change feature

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ var featureWidget = {
       $(".features__content").removeClass("features__content--active");
       let showThisContent = "#" + $(this).attr("data-toggler");
       $(showThisContent).addClass("features__content--active");
+
+      let showThisImage = $(this).attr("data-image");
+      $(".features__left").css("background-image", "url('../images/" + showThisImage + "')");
+      console.log(showThisImage);
     });
   }
 }
